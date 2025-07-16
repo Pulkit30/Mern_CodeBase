0de61a392b673c3987a1f68d67ca0a3f4dfc2228
pulkitSharma <pulkitsharma1816@gmail.com>
2025-07-17 00:34:54 +0530

    react basic projects

---
+++ ReactJs Projects/14 - Initialising Create React App/src/App.js
@@ -0,0 +1,10 @@
+import "./App.css";
+
+
+const App = ()=>(
+  <div className="App App-header ">
+      Welcome to React Course on codingninjas.com
+  </div>
+)
+
+export default App;
---
+++ ReactJs Projects/16 - NFTium/src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+import Navbar from "./Navbar.js";
+import Hero from "./Hero.js";
+import About from "./About.js";
+export default function App() {
+  // Write this code in Navbar.js file
+
+  // Write this code in Hero.js file
+
+  // Write this code in About.js file
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+  );
+}
